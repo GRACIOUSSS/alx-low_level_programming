7374ec19ad55307c3de8906c0c5bc7f5a7faf803
GRACIOUSSS <tobiolaoluwa061@gmail.com>
2022-10-15 00:54:06 -0700

    answer to task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point for task 5 program.
+ *
+ * Return: zero if no errors, return non zero if errors
+ */
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
