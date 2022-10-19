396ec2dea471cabaaecd4e8f10ac52165f7bdb2e
GRACIOUSSS <tobiolaoluwa061@gmail.com>
2022-10-19 14:09:10 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include <unistd.h>
 #include "main.h"
 /**
- * main - check description
- * Description: it prints the word _putchar, followed by a new line.
- * Return: zero
+ * main - Prints Holberton as a message.
+ *
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
 
-	char word[8] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-		_putchar(word[i]);
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
 	_putchar('\n');
-
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,16 @@
-#include <stdio.h>
-/**
- * main - entry point
- *
- * Return: Always zero (success)
- */
-int main(void)
-{
-	char *sh = "stdio.h";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
-
-	return (0);
-}
+#ifndef main_h
+#define main_h
+extern int _putchar(char c);
+extern void print_alphabet(void);
+extern void print_alphabet_x10(void);
+extern int _islower(int c);
+extern int _isalpha(int c);
+extern int print_sign(int n);
+extern int _abs(int);
+extern int print_last_digit(int);
+extern void jack_bauer(void);
+extern void times_table(void);
+extern int add(int, int);
+extern void print_to_98(int n);
+extern void print_times_table(int n);
+#endif
