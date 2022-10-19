385e4b19c7b8d2635d2153661fb46189af4b67db
GRACIOUSSS <tobiolaoluwa061@gmail.com>
2022-10-19 14:34:13 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,15 @@ void print_alphabet_x10(void)
 	char i, k;
 
 	for (k = 0; k < 10; k++)
-{
-	for (i = 'a'; i <= 'z'; i++)
 	{
-		_putchar(i);
+
+	
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
+
+ 
